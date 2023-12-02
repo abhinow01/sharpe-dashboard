11590af9d6a896e24583d6f8849c662c37cf0ea6
abhinav khanna <kabhinav99@gmail.com>
2023-12-02 20:31:13 +0530

    tailwind config

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+// tailwind.config.js
+// tailwind.config.cjs
+
+export default {
+  content: ['./index.html', './src/**/*.{js,jsx,ts,tsx}'],
+  theme: {
+    extend: {
+      // Add custom theme configurations or extend the default theme here
+    },
+  },
+  plugins: [
+    // Add custom plugins or use built-in Tailwind plugins here
+  ],
+}
